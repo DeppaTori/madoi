6886fe2029e12322062d7a8c8ff36580bee32726
Tommy Toban <tommy.deppatori@gmail.com>
2019-06-29 11:29:51 +0700

    add component and style

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
+	import MyComponent from './MyComponent.svelte';
 	let name = 'world';
+	let src = `favicon.png`;
 </script>
 
+<style>
+	p {
+		color: red;
+		font-family: 'Comic Sans MS';
+		font-size: 2em;
+	}
+</style>
+
 <h1>Hello {name}!</h1>
+<img {src} alt="Svelte Icon" >
+<p>This is a paragraph.</p>
+<MyComponent />
